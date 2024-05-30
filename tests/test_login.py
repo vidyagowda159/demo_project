23eb58b6d09bfa38800a6ef33de7c7f932a952c6
vidyagowda159 <vidya@gmail.com>
2024-05-30 11:15:38 +0530

    taking screenshots

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pytest
 from POM.login_page import LoginPage
 
@@ -7,6 +8,11 @@ def test_if_user_is_able_to_login(driver_initializer, env_config):
 	driver = driver_initializer
 	l_obj = LoginPage(driver)
 	l_obj.login(env_config.username, env_config.password)
+	dt = datetime.now()
+	date = f'{dt.year}-{dt.day}-{dt.month}'
+	time_ = f"{dt.hour}-{dt.minute}-{dt.second}"
+
+	driver.save_screenshot(f"login_page{date}-{time_}.png")
 	l_obj.is_user_logged_in()
 
 
